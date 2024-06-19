f07d067f260b59d8e8c2863fe6b48fead169d460
mariaclarinha27 <gokujosiana@gmail.com>
2024-06-19 23:49:43 +0000

    19/06/2024

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
   {
-    enunciado: "Qual é a cor dos olhos do Dayvison?",
+    enunciado: "Assim que saiu da escola, você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pesssoa pode ter. Ele também gera imagens e audios super realistas. Qual o seu primeiro pensamento?",
     alternativas: [
-      "Azuis",
-      "Verdes"
+      "Isso é assustador!",
+      "Isso é maravilhoso!"
     ]
   },
 
   {
-    enunciado: "Quem é o melhor vilão Jason ou Fred Krügger?",
+    enunciado: "Com a descoberta desta tecnologia chamada inteligência artificial (IA), uma professora de tecnologia de uma escola decidiu fazer uma sequência de aulas sobre essa ferramenta. No fim de uma aula, ela pede que você escreva um trabalho sobre o uso da IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Jason",
-      "Fred"
+      "Utiliza uma ferramenta de busca de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique em uma linguagem que facilite o entendimento.",
+      "Escreve o trabalho com base nas conversas que teve com os colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
     ]
   },
 
